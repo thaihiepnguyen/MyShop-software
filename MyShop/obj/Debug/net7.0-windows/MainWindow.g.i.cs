6c0a4b8e25703b9953c3d22b41d021d615a8f305
thaihiepnguyen <thaihiep232002@gmail.com>
2023-04-06 00:29:17 +0700

    Apply MVC

--- MyShop/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ MyShop/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace MyShop {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MyShop;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MyShop;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
