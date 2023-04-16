e99bfaa38f137b7173b0186d4e31af0f2225e484
TanKaka123 <80939836+TanKaka123@users.noreply.github.com>
2023-04-16 15:51:16 +0700

    Add files via upload

---
+++ MyShop/DTO/UserDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DTO
+{
+    internal class UserDTO
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Fullname { get; set; }
+        // 0 -> male, 1 -> female
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string Tel { get; set; }
+        public string AvatarPath { get; set; }
+        public byte IsHide { get; set; }
+        public int? RoleID { get; set; }
+    }
+}
