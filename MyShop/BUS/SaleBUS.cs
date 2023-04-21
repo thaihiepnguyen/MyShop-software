7314a51bb39e557e8141ac8aceead736c97790b7
TanKaka123 <80939836+TanKaka123@users.noreply.github.com>
2023-04-22 05:04:15 +0700

    Add files via upload

---
+++ MyShop/BUS/SaleBUS.cs
@@ -0,0 +1,32 @@
+﻿using MyShop.DAO;
+using MyShop.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.BUS
+{
+    internal class SaleBUS
+    {
+        public SaleBUS()
+        {
+           
+        }
+        private SaleDAO saleDAO = new SaleDAO();
+
+        public decimal getSaleByYear(int userID, int year)
+        {
+            return saleDAO.getSaleByYear(userID, year);
+        }
+        public decimal getSaleByMonth(int userID, int month)
+        {
+            return saleDAO.getSaleByMonth(userID, month);
+        }
+        public decimal getSaleByDate(int userID)
+        {
+            return saleDAO.getSaleByDate(userID);
+        }
+    }
+}
