2e90ec32f3606cecffd2470d13cf0d98d983a3a7
thaihiepnguyen <thaihiep232002@gmail.com>
2023-04-23 22:39:33 +0700

    update db_ver5

--- MyShop/UI/MainPage/Pages/AddOrder.xaml.cs
+++ MyShop/UI/MainPage/Pages/AddOrder.xaml.cs
@@ -2,23 +2,10 @@
 using MyShop.DTO;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Collections.ObjectModel;
-using System.Xml.Schema;
-using DocumentFormat.OpenXml.Bibliography;
-using DocumentFormat.OpenXml.VariantTypes;
-using DocumentFormat.OpenXml.Office2010.ExcelAc;
 
 namespace MyShop.UI.MainPage.Pages
 {
